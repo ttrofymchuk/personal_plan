4cc3fdeeddadb8c78f632249700b83b91bf39ad8
taras <taras@laptop.(none)>
2010-12-13 00:50:06 +0200

    initial commit

---
+++ personal_plan/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PersonalPlan::Application.config.secret_token = '023210d23fa62ef4eb9b79e759c6a4eaa1daf50b3420d48921610d059683f0e308f900e9199eb25afb4164aabdbd1ca951da78b714b50d52fbf579a98fa30dae'
